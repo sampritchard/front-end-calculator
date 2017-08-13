67fbcaa153d9eebb642ec2599d87ddf2f01fc833
Sam Pritchard <sampritchard@msn.com>
2017-08-13 15:47:59 +0100

    Arithmetic tests for sum function

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -12,4 +12,34 @@ describe('Calculator', function() {
   it('starts with an empty array for history', function() {
     expect(calculator.history).toEqual([]);
   })
+
+  it('can do basic addition', function() {
+    calculator.history = [11+11]
+    calculator.sum();
+    expect(calculator.result).toEqual(22);
+  })
+
+  it('can do basic subtraction', function() {
+    calculator.history = [21-11]
+    calculator.sum();
+    expect(calculator.result).toEqual(10);
+  })
+
+  it('can do basic multiplication', function() {
+    calculator.history = [2*2]
+    calculator.sum();
+    expect(calculator.result).toEqual(4);
+  })
+
+  it('can do basic division', function() {
+    calculator.history = [4/2]
+    calculator.sum();
+    expect(calculator.result).toEqual(2);
+  })
+
+  it('can understand operator precedance', function() {
+    calculator.history = [1+2*3]
+    calculator.sum();
+    expect(calculator.result).toEqual(7);
+  })
 })
